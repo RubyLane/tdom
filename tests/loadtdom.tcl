854b34fb12f4e3881f8b5be92848091fc236b43e
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2026-04-27 10:23:35 -0300

    Test loading fixes

--- tests/loadtdom.tcl
+++ tests/loadtdom.tcl
@@ -11,16 +11,6 @@ package require tcltest 2.2-
 namespace import ::tcltest::*
 catch {tcltest::loadTestedCommands}
 
-if {[catch {package require -exact tdom 0.9.6.1}]} {
-    if {[package vsatisfies [package provide Tcl] 9.0-]} {
-        set libname libtcl9tdom0.9.6.1[info sharedlibextension]
-    } else {
-        set libname libtdom0.9.6.1[info sharedlibextension]
-    }
-    if {[catch {load [file join [file dir [info script]] ../unix/$libname]}]} {
-        error "Unable to load the appropriate tDOM version!"
-    }
-}
 if {[info commands ::tdom::xmlReadFile] == ""} {
     # tcldomsh without the script library. Source the lib.
     source [file join [file dir [info script]] ../lib tdom.tcl]
